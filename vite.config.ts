0019c29dff8bf747800b33a53ea4804905b74019
Компьютер <vitamin48>
2025-04-30 17:01:45 +0300

    Подготовка к деплою на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-});
+  base: '/bothubster/', // 👈 имя репозитория на GitHub
+  plugins: [react()],
+})
