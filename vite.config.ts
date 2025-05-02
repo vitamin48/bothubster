9f6f25acdf5f96ce6c7351fe822417d69c201bf1
Компьютер <vitamin48>
2025-05-02 11:02:39 +0300

    vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bothubster/', // 👈 имя репозитория на GitHub
+  base: '/', 
   plugins: [react()],
 })
